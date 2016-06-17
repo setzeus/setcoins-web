f2a8c4d8a5e3e32d3fea4f21941886dc91290f8a
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-06-17 05:06:30 -0400

    mobile responsive

--- src/components/SendBTC.jsx
+++ src/components/SendBTC.jsx
@@ -21,13 +21,12 @@ export default class SendBTc extends Base {
 	
 		return (
 			<div className='section' >
-				<div className='send-scene'>
-					<div className='send-info'>
+				<div className='map-scene'>
+					<div className='map-info'>
 						<div className='content-container'>
 							<h1 className='content-title'>Title</h1>
 							<p className='content'>Contnet content content content content content </p>
 						</div>
-						
 						<div className='photo-container'>
 							<img className='screen-shot send-btc' src='img/send-btc.jpg'></img>
 						</div>	
