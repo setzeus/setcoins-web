9be05e9f3c42ab7ffa28c7865a6c6471a5329f25
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-06-22 10:42:06 -0400

    working on safari added webpack plugin for fetch

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ var webpack = require('webpack');
 module.exports = {
 	devtool: 'eval',
 	entry: [
-	'babel-polyfill',
 	'./src/styles/index.less',
 	'./src/index.jsx',
 	'webpack-dev-server/client?http://localhost:8080'
@@ -14,7 +13,11 @@ module.exports = {
 		path: path.resolve(__dirname,'public')
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.ProvidePlugin({
+			'Promise': 'exports?global.Promise!es6-promise',
+			'fetch': 'exports?self.fetch!whatwg-fetch'
+		})
 	],
 	module: {
 		loaders: [
