36ead39f21d7fd69a0ede443bb1c09d6dc5a13c0
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-06-30 17:48:43 -0400

    feel the new economy full responsive

--- src/components/ScreenShots.jsx
+++ src/components/ScreenShots.jsx
@@ -36,7 +36,7 @@ export default class ScreenShots extends Base {
 				</div>
 				<div className='section shotsb' >
 					<div className='wrapper-screen'>
-						<img className='screen-shots' src='http://d1wbxby8dwa4u.cloudfront.net/namecheap/Withdraw-USD-polished.png'></img>
+						<img className='screen-shots' src='http://d1wbxby8dwa4u.cloudfront.net/namecheap/withdraw.png'></img>
 					</div>
 				</div>
 			</div>
